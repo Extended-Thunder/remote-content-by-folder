4aa572a27ea2b84363f8cc3ce88e321bf011e7de
Jonathan Kamens <jik@extended-thunder.org>
2023-08-11 17:12:53 -0400

    Clean up preferences handling and make debug logging a preference
    * Add a new preference which controls debug logging.
    * Remove all traces of the old `logging.console` and `logging.dump`
      preferences, which are no longer being used.
    * Create a `getPref` function which does the heavy lifting and invoke
      it from the other stuff that needs preferences, rather than using
      the full `browser.LegacyPrefs.getPref` interface.
    * Don't fetch prefs in `init` into an array that just gets thrown
      away.

--- background.js
+++ background.js
@@ -1,20 +1,32 @@
-const DEBUG = false;
 const PREF_PREFIX = "extensions.remote-content-by-folder.";
 
+const debugPref = "debug";
 const allowPref = "allow_regexp";
 const blockPref = "block_regexp";
 const blockFirstPref = "block_first";
 
 const PREF_DEFAULTS = {
-    "logging.console": "Warn", // Unused
-    "logging.dump": "Fatal", // Unused
+    [debugPref]: false,
     [allowPref]: "",
     [blockPref]: "",
     [blockFirstPref]: false,
 }
 
-function debug(msg) {
-    if (DEBUG) {
+async function getPref(name) {
+    let fullName = PREF_PREFIX + name;
+    return await browser.LegacyPrefs.getPref(fullName);
+}
+
+async function debug(msg) {
+    let debug;
+    try {
+        debug = await getPref(debugPref);
+    }
+    catch (ex) {
+        console.log("Failed to fetch debug pref, defaulting to true", ex);
+        debug = true;
+    }
+    if (debug) {
         console.log("RCBF:", msg);
     }
 }
@@ -24,7 +36,6 @@ async function init() {
     let prefs = {};
     for (let [name, value] of Object.entries(PREF_DEFAULTS)) {
         await browser.LegacyPrefs.setDefaultPref(`${PREF_PREFIX}${name}`, value);
-        prefs[name] = await browser.LegacyPrefs.getPref(`${PREF_PREFIX}${name}`);
     }
 
     messenger.messages.onNewMailReceived.addListener(checkNewMessages);
@@ -39,20 +50,20 @@ async function checkNewMessages(folder, messages) {
 async function checkMessage(message) {
     let currentPolicy = await browser.RemoteContent.getContentPolicy(message.id);
     if (currentPolicy != "None") {
-        debug(`Content policy for message "${message.id}" is set to "${currentPolicy}", not modifying`);
+        await debug(`Content policy for message "${message.id}" is set to "${currentPolicy}", not modifying`);
         return;
     }
 
     // Get newPolicy from regex match.
     let requestedPolicy = await getPolicyFromRegExMatch(message);
     if (requestedPolicy && currentPolicy != requestedPolicy) {
-        debug(`Switching content policy for message "${message.id}" from "${currentPolicy}" to "${requestedPolicy}"`);
+        await debug(`Switching content policy for message "${message.id}" from "${currentPolicy}" to "${requestedPolicy}"`);
         await browser.RemoteContent.setContentPolicy(message.id, requestedPolicy);
     }
 };
 
 async function getPolicyFromRegExMatch(message) {
-    let blockFirst = await browser.LegacyPrefs.getPref(`${PREF_PREFIX}${blockFirstPref}`);
+    let blockFirst = await getPref(blockFirstPref);
     if (blockFirst) {
         if (await checkRegexp(message, blockPref)) {
             return "Block";
@@ -73,16 +84,16 @@ async function getPolicyFromRegExMatch(message) {
 }
 
 async function checkRegexp(msgHdr, prefName) {
-    let regexp = await browser.LegacyPrefs.getPref(`${PREF_PREFIX}${prefName}`);
+    let regexp = await getPref(prefName);
     if (regexp != "") {
         try {
             let regexpObj = new RegExp(regexp);
-            debug(`Testing ${prefName} regexp "${regexp}" against folder name "${msgHdr.folder.name}"`);
+            await debug(`Testing ${prefName} regexp "${regexp}" against folder name "${msgHdr.folder.name}"`);
             if (regexpObj.test(msgHdr.folder.name)) {
-                debug(`${prefName} regexp "${regexp}" matched folder name "${msgHdr.folder.name}"`);
+                await debug(`${prefName} regexp "${regexp}" matched folder name "${msgHdr.folder.name}"`);
                 return true;
             }
-            debug(`${prefName} regexp "${regexp}" did not match folder name "${msgHdr.folder.name}"`);
+            await debug(`${prefName} regexp "${regexp}" did not match folder name "${msgHdr.folder.name}"`);
             return false;
         } catch (ex) {
             console.error(`Invalid regexp: "${regexp}"`);
@@ -90,7 +101,7 @@ async function checkRegexp(msgHdr, prefName) {
         }
     }
 
-    debug(`${prefName} is empty, not testing`);
+    await debug(`${prefName} is empty, not testing`);
     return false;
 }
 
